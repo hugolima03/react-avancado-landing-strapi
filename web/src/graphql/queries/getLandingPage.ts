493e031884562fa1b413a55112ddff77f0c5f8d4
Hugo Lima Romão <hugo8romao@gmail.com>
2022-03-10 16:21:22 -0400

    sectionTech and sectionConcepts

--- web/src/graphql/queries/getLandingPage.ts
+++ web/src/graphql/queries/getLandingPage.ts
@@ -32,11 +32,34 @@ const GET_LANDING_PAGE = /* GraphQL */ `
     }
   }
 
+  fragment sectionTech on LandingPage {
+    sectionTech {
+      title
+      techIcons {
+        icon {
+          url
+        }
+        title
+      }
+    }
+  }
+
+  fragment sectionConcepts on LandingPage {
+    sectionConcepts {
+      title
+      concepts {
+        title
+      }
+    }
+  }
+
   query GET_LANDING_PAGE {
     landingPage {
       ...logo
       ...header
       ...sectionAboutProject
+      ...sectionTech
+      ...sectionConcepts
     }
   }
 `
--- web/src/types/api.ts
+++ web/src/types/api.ts
@@ -25,8 +25,29 @@ export type SectionAboutProjectProps = {
   }
 }
 
+export type TechIcon = {
+  title: string
+  icon: {
+    url: string
+  }
+}
+
+export type SectionTechProps = {
+  title: string
+  techIcons: TechIcon[]
+}
+
+export type SectionConceptsProps = {
+  title: string
+  concepts: {
+    title: string
+  }[]
+}
+
 export type LandingPageProps = {
   logo: LogoProps
   header: HeaderProps
   sectionAboutProject: SectionAboutProjectProps
+  sectionTech: SectionTechProps
+  sectionConcepts: SectionConceptsProps
 }
